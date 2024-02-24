a764eb8549cc07f0c54d1f4de783e1a4152adf72
Amelievne <amelie.vne@gmail.com>
2024-02-24 16:36:09 +0100

    Added JS for Tokyo time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+let tokyoElem = document.querySelector("#tokyo");
+let tokyoDateElem = tokyoElem.querySelector(".date");
+let tokyoTimeElem = tokyoElem.querySelector(".time");
+let tokyoAmPmElem = document.querySelector("#tokyo .am-pm");
+let tokyoTime = moment();
 
+tokyoDateElem.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElem.innerHTML = tokyoTime.format("hh:mm");
+tokyoAmPmElem.innerHTML = tokyoTime.format("A");
